9b8684ce29051fe52724956691930621961dd527
nely de faria <nelly.nf54@gmail.com>
2021-06-01 12:47:09 -0300

    Adiciona arquivo modificadores.cpp

---
+++ c-plus-plus/modificadores.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+    cout << "Tamanho char: " << sizeof(char) << endl;
+    cout << "Tamanho int: " << sizeof(int) << endl;
+    cout << "Tamanho short int: " << sizeof(short int) << endl;
+    cout << "Tamanho long int: " << sizeof(long int) << endl;
+    cout << "Tamanho long long int: " << sizeof(long long int) << endl;
+    cout << "Tamanho float: " << sizeof(float) << endl;
+    //cout << "Tamanho long float: " << sizeof(long float) << endl;
+    cout << "Tamanho double: " << sizeof(double) << endl;
+    cout << "Tamanho long double: " << sizeof(long double) << endl;
+    cout << "Tamanho wchar_t: " << sizeof(wchar_t) << endl;
+
+}

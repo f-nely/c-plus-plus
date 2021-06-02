f3d3807c8266fa423a4483361adf2266cd7800e3
nely de faria <nelly.nf54@gmail.com>
2021-06-02 15:59:43 -0300

    Adiciona arquivo pares_while.cpp

---
+++ c-plus-plus/pares_while.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+    int j = 0;
+
+    while (j <= 50) {
+
+        if (j % 2 == 0) {
+
+            cout << j << endl;
+
+        }
+
+        j++;
+    }
+
+}

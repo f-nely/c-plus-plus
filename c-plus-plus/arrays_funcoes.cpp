a31fdfc84ef6fc5312a4e7641a37eb01e8ae2e9f
nely de faria <nelly.nf54@gmail.com>
2021-06-07 11:18:35 -0300

    Criando função alterarVetor

--- c-plus-plus/arrays_funcoes.cpp
+++ c-plus-plus/arrays_funcoes.cpp
@@ -2,25 +2,37 @@
 
 using namespace std;
 
-void mostrarVetor(int Array[], int tamanhoVetor);
+void mostrarVetor(int *ptr_array, int tamanhoVetor);
+void alterarVetor(int Array[], int tamanhoVetor);
 
 int main()
 {
     int vetor[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
     int tamanhoVetor = sizeof(vetor) / sizeof(int);
-    //cout << "Tamanho vetor: " << tamanhoVetor;
 
+    cout << "\nVetor Original";
+    mostrarVetor(vetor, tamanhoVetor);
+    alterarVetor(vetor, tamanhoVetor);
+    cout << "\nVetor Alterado?";
     mostrarVetor(vetor, tamanhoVetor);
 
     return 0;
 }
 
-void mostrarVetor(int Array[], int tamanhoVetor)
+void mostrarVetor(int *ptr_array, int tamanhoVetor)
 {
     for (int i = 0; i < tamanhoVetor; i++) {
 
-        cout << "- " << Array[i] << " -";
+        cout << "- " << ptr_array[i] << " -"; // ptr_array[i] é o mesmo que *(ptr_array + i)
+    }
+    cout << "\n\n";
+}
 
+void alterarVetor(int Array[], int tamanhoVetor)
+{
+    for (int i = 0; i < tamanhoVetor; i++) {
+
+        Array[i] *= 2;
     }
 }
